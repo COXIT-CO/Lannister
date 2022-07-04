d64d642415aae2d43e4eb76941ef844169748a3e
peacedetskiy <poopseek1337@gmail.com>
2022-07-04 22:58:19 +0300

    Edit sql-script according to an updated DB design

--- lannister.sql
+++ lannister.sql
@@ -11,29 +11,40 @@ CREATE TYPE "request_status" AS ENUM (
   'done'
 );
 
-CREATE TABLE "users" (
-  "id" varchar(12) PRIMARY KEY,
-  "name" varchar(80) NOT NULL,
-  "email" varchar(40) NOT NULL,
+CREATE TABLE "user" (
+  "id" character(12) PRIMARY KEY,
+  "name" character(80) NOT NULL,
+  "email" character(40) NOT NULL,
   "roles" user_role[] NOT NULL
 );
 
-CREATE TABLE "requests" (
+CREATE TABLE "request" (
   "id" SERIAL PRIMARY KEY,
-  "creator" varchar(12),
-  "reviewer" varchar(12),
-  "bonus_type" varchar(40) NOT NULL,
-  "description" text,
-  "status" request_status,
-  "date_created" timestamp NOT NULL DEFAULT now(),
-  "date_changed" timestamp NOT NULL DEFAULT now(),
+  "creator" character(12),
+  "reviewer" character(12),
+  "bonus_type" character(40) NOT NULL,
+  "description" text
+);
+
+CREATE TABLE "request_history" (
+  "request_id" int PRIMARY KEY,
+  "status" request_status NOT NULL DEFAULT 'created',
+  "date_created" timestamp NOT NULL,
+  "date_approved" timestamp,
+  "date_rejected" timestamp,
+  "date_done" timestamp,
+  "date_changed" timestamp,
   "date_payment" timestamp
 );
 
-ALTER TABLE "requests" ADD FOREIGN KEY ("creator") REFERENCES "users" ("id");
+ALTER TABLE "request" ADD FOREIGN KEY ("creator") REFERENCES "user" ("id");
 
-ALTER TABLE "requests" ADD FOREIGN KEY ("reviewer") REFERENCES "users" ("id");
+ALTER TABLE "request" ADD FOREIGN KEY ("reviewer") REFERENCES "user" ("id");
 
-ALTER TABLE "users" ADD CONSTRAINT "roles_overflow" CHECK (array_length("roles", 1) <= 3);
+ALTER TABLE "request_history" ADD FOREIGN KEY ("request_id") REFERENCES "request" ("id");
 
-ALTER TABLE "requests" ADD CONSTRAINT "exclude_self_request" CHECK ("creator" <> "reviewer");
+ALTER TABLE "user" ADD CONSTRAINT "roles_overflow" CHECK (array_length("roles", 1) <= 3);
+
+ALTER TABLE "request" ADD CONSTRAINT "exclude_self_request" CHECK ("creator" <> "reviewer");
+
+ALTER TABLE "request_history" ADD CONSTRAINT "exclude_opposite_status" CHECK (NOT ("date_approved" IS NOT NULL AND "date_rejected" IS NOT NULL));
