98877d875337a14decfd13fd0ba3ce9f72b728a5
¨NeteliMaksymuk¨ <natavaksumyk@gmail.com>
2022-07-12 16:21:13 +0300

    Change the project structure

--- Lannister/api_src/LannisterProject/settings.py
+++ Lannister/api_src/LannisterProject/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,10 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g(-86&a8c69*e^0wu^p#vw$1g68svrhjzlfi(33s1vw(_(%@1q'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with dSECRET_KEY = ebug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0']
@@ -76,11 +77,7 @@ WSGI_APPLICATION = 'LannisterProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgresql',
-        'USER': 'postgresql',
-        'PASSWORD': 'postgresql',
-        'HOST': 'lannister_db',
-        'PORT': 5432,
+
     }
 }
 
