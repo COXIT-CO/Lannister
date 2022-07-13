9adecec4d0c864a3e21329c9c04d43ea493549f0
¨NeteliMaksymuk¨ <natavaksumyk@gmail.com>
2022-07-13 19:46:50 +0300

    Fix black and isort

--- Lannister/api_src/manage.py
+++ Lannister/api_src/manage.py
@@ -1,4 +1,3 @@
-
 """
 Django administration utility.
 """
@@ -7,9 +6,9 @@ import sys
 
 PWD = os.path.abspath(os.path.dirname(__file__))
 
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'LannisterProject.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "LannisterProject.settings")
     sys.path.append(PWD)
     try:
         from django.core.management import execute_from_command_line
