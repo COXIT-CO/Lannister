40cc2e77e1ff72e884201332dd787467a004dd78
¨NeteliMaksymuk¨ <natavaksumyk@gmail.com>
2022-07-13 11:58:44 +0300

    delete .idea and change manage.py

--- Lannister/api_src/manage.py
+++ Lannister/api_src/manage.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+
+"""
+Django administration utility.
+"""
 import os
 import sys
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LannisterProject.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+PWD = os.path.abspath(os.path.dirname(__file__))
 
 if __name__ == '__main__':
-    main()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LannisterProject.settings')
+    sys.path.append(PWD)
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        try:
+            import django  # pylint: disable=unused-import
+        except ImportError as error:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from error
+        raise
+    execute_from_command_line(sys.argv)
