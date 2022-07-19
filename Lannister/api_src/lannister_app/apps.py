45642f71023b7cec7fbcd5a17541031bbfe9cda9
Andrew Nosenko <andrewnoa20@gmail.com>
2022-07-19 15:32:16 +0300

    Fix lannister_app folder files with black and flake8

--- Lannister/api_src/lannister_app/apps.py
+++ Lannister/api_src/lannister_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class LannisterAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lannister_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "lannister_app"
