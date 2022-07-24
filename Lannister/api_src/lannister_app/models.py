de1dc9b051446c2ea251de35973c512577f73500
¨NeteliMaksymuk¨ <natavaksumyk@gmail.com>
2022-07-24 19:06:15 +0300

    Сorrection of insort errors

--- Lannister/api_src/lannister_app/models.py
+++ Lannister/api_src/lannister_app/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.postgres.fields import ArrayField
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class MyUserManager(BaseUserManager):
