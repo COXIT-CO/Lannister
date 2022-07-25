cb93eaece619909ad7599150d4887c2c7cb512b9
¨NeteliMaksymuk¨ <natavaksumyk@gmail.com>
2022-07-25 14:26:04 +0300

    Fix bug errors in models.py

--- Lannister/api_src/lannister_app/models.py
+++ Lannister/api_src/lannister_app/models.py
@@ -19,24 +19,12 @@ class MyUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self,
-                    username,
-                    email,
-                    password=None,
-                    **extra_fields):
+    def create_user(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(username,
-                                 email,
-                                 password,
-                                 **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
-    def create_superuser(self,
-                         username,
-                         roles,
-                         email,
-                         password,
-                         **extra_fields):
+    def create_superuser(self, username, roles, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         if extra_fields.get("is_staff") is not True:
@@ -44,24 +32,23 @@ class MyUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(username,
-                                 roles,
-                                 email,
-                                 password,
-                                 **extra_fields)
+        return self._create_user(username, roles, email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Users within the Django authentication system are represented by this
+    model.
+    """
+
     username = models.CharField(_("username"), max_length=30)
     email = models.EmailField(_("email address"), unique=True)
-    roles = ArrayField(models.CharField(_("roles"), max_length=20),
-                       default=["worker"])
+    roles = ArrayField(models.CharField(_("roles"), max_length=20), default=["worker"])
 
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
-        help_text=_("Designates whether the user "
-                    "can log into this admin site."),
+        help_text=_("Designates whether the user " "can log into this admin site."),
     )
     is_active = models.BooleanField(
         _("active"),
