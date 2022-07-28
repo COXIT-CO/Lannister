750fc9a722cb29d6a0dcb54a09de883ffd55a2f3
Andrew Nosenko <andrewnoa20@gmail.com>
2022-07-28 20:13:10 +0300

    Fix path import

--- Lannister/api_src/LannisterProject/urls.py
+++ Lannister/api_src/LannisterProject/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from lannister_app import views
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
