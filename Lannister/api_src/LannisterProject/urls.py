daa022a68f15a3150a5f31f7f625e58bbccddc13
Andrew Nosenko <andrewnoa20@gmail.com>
2022-07-28 20:01:07 +0300

    Sort url according to linter

--- Lannister/api_src/LannisterProject/urls.py
+++ Lannister/api_src/LannisterProject/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from lannister_app import views
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
-
-from lannister_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
