acdd9199020401a047295949e0a02a45517cc741
Andrew Nosenko <andrewnoa20@gmail.com>
2022-07-28 16:39:35 +0300

    fix with black and isort

--- Lannister/api_src/lannister_app/apps.py
+++ Lannister/api_src/lannister_app/apps.py
@@ -6,4 +6,4 @@ class LannisterAppConfig(AppConfig):
     name = "lannister_app"
 
     def ready(self):
-        import lannister_app.signals
+        import lannister_app.signals # noqa
--- Lannister/api_src/lannister_app/urls.py
+++ Lannister/api_src/lannister_app/urls.py
@@ -17,11 +17,29 @@ from .views import (
 
 urlpatterns = [
     path("requests/", BonusRequestList.as_view(), name="bonus_request-list"),
-    path("requests/<int:pk>", BonusRequestSingle.as_view(), name="bonus_request-single"),
-    path("requests/worker/<int:pk>", WorkerBonusRequestList.as_view(), name="worker-bonus_request-list"),
-    path("requests/reviewer/<int:pk>", ReviewerBonusRequestList.as_view(), name="reviewer-bonus_request-list"),
-    path("requests_history/", BonusRequestHistoryList.as_view(), name="bonus_request_history-list"),
-    path("requests_history/<int:pk>", BonusRequestHistorySingle.as_view(), name="bonus_request_history-single"),
+    path(
+        "requests/<int:pk>", BonusRequestSingle.as_view(), name="bonus_request-single"
+    ),
+    path(
+        "requests/worker/<int:pk>",
+        WorkerBonusRequestList.as_view(),
+        name="worker-bonus_request-list",
+    ),
+    path(
+        "requests/reviewer/<int:pk>",
+        ReviewerBonusRequestList.as_view(),
+        name="reviewer-bonus_request-list",
+    ),
+    path(
+        "requests_history/",
+        BonusRequestHistoryList.as_view(),
+        name="bonus_request_history-list",
+    ),
+    path(
+        "requests_history/<int:pk>",
+        BonusRequestHistorySingle.as_view(),
+        name="bonus_request_history-single",
+    ),
     path("users/", UserList.as_view(), name="user-list"),
     path("users/<int:pk>", UserSingle.as_view(), name="user-single"),
     path("reviewers/", ReviewerList.as_view(), name="reviewers-list"),
